546d630fb3f2a29b567f8c9ece8b45fe14f49114
Jason <enigsuss@gmail.com>
2024-07-16 16:49:21 +0900

    babel & webpack 완료

---
+++ 2-babel-webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        library: 'lib',
+    },
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
